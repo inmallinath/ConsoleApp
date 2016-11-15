f1b9b41fa0257f8b7bca643de591d3255660967a
Mallinath Nagaraj <inmallinath@hotmail.com>
2016-11-14 17:23:54 -0800

    add the models and the sql connection

---
+++ BloggingContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApplication
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts {get; set; }
+
+       protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+       {
+           var connection = @"Server=.\sqlexpress;Database=ConsoleApp;Trusted_Connection=True";
+           optionsBuilder.UseSqlServer(connection);
+       }
+    }
+
+    
+}
