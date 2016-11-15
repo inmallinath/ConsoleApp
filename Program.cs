0ce46318d28a5ff3c4aeafd39700aade6bcbb754
Mallinath Nagaraj <inmallinath@hotmail.com>
2016-11-15 10:53:21 -0800

    add code to save and retrieve a sample blog url

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using(var db = new BloggingContext())
+            {
+                db.Add(new Blog { Url = "blogs.msdn.com" });
+                var count = db.SaveChanges();
+                Console.WriteLine("----SAVE-----");
+                Console.WriteLine($"{count} records saved to the database");
+
+                Console.WriteLine("----RETRIEVE-----");
+                foreach(var blog in db.Blogs)
+                    Console.WriteLine($"- {blog.Url}");
+            }
         }
     }
 }
